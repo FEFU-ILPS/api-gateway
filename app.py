b95a2681311fdefa4d71533e6138982a5469fe4e
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-03-03 22:52:38 +1000

    Изменен скрипт запуска приложения

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from contextlib import asynccontextmanager
 
+import uvicorn
 from fastapi import FastAPI
 
-from routers import health_router, auth_router, audio_router, text_router
+from config import configs
+from routers import audio_router, auth_router, health_router, text_router
 
 
 @asynccontextmanager
@@ -19,3 +21,14 @@ app.include_router(health_router)
 app.include_router(auth_router)
 app.include_router(audio_router)
 app.include_router(text_router)
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "app:app",
+        host="0.0.0.0",
+        port=8061,
+        reload=configs.DEBUG_MODE,
+        use_colors=True,
+        proxy_headers=True,
+    )
