b43daaa66ac4af1795af5d0892ca56b9178d424f
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-03-03 22:43:20 +1000

    Удален лишний раздел

--- config/__init__.py
+++ config/__init__.py
@@ -1,11 +1,8 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from .upload import UploadSettings
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_prefix="BACKEND_")
-
-    upload = UploadSettings()
 
     # Опциональные переменные
     DEBUG_MODE: bool = False
--- config/upload.py
+++
@@ -1,8 +0,0 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
-
-
-class UploadSettings(BaseSettings):
-    model_config = SettingsConfigDict(env_prefix="BACKEND_UPLOAD_")
-
-    # Опциональные переменные
-    DIR: str = "uploads"
