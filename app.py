f61941555e03ef14ee632063250e42145803c8c3
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-03-14 21:36:36 +1000

    Убрано лишнее

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 from contextlib import asynccontextmanager
 
-import uvicorn
 from fastapi import FastAPI
 
-from configs import configs
-from routers import audio_router, auth_router, health_router, text_router
+from routers import auth_router, health_router
 
 
 @asynccontextmanager
-async def server_lifespan(app: FastAPI):
+async def server_lifespan(_: FastAPI):
     # До запуска приложения
 
     yield
@@ -16,19 +14,6 @@ async def server_lifespan(app: FastAPI):
     # После запуска
 
 
-app = FastAPI(lifespan=server_lifespan)
-app.include_router(health_router)
-app.include_router(auth_router)
-app.include_router(audio_router)
-app.include_router(text_router)
-
-
-if __name__ == "__main__":
-    uvicorn.run(
-        "app:app",
-        host="0.0.0.0",
-        port=8061,
-        reload=configs.DEBUG_MODE,
-        use_colors=True,
-        proxy_headers=True,
-    )
+gateway = FastAPI(lifespan=server_lifespan)
+gateway.include_router(health_router)
+gateway.include_router(auth_router)
