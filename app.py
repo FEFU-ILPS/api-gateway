de6a80b7f7135407f0f9b6bda9e8accc297bd688
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-03-15 00:42:39 +1000

    CORS

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routers import auth_router, health_router
 
 
 @asynccontextmanager
-async def server_lifespan(_: FastAPI):
+async def lifespan(_: FastAPI):
     # До запуска приложения
 
     yield
@@ -14,6 +15,14 @@ async def server_lifespan(_: FastAPI):
     # После запуска
 
 
-gateway = FastAPI(lifespan=server_lifespan)
+gateway = FastAPI(lifespan=lifespan)
 gateway.include_router(health_router)
 gateway.include_router(auth_router)
+
+gateway.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # TODO: Изменить в проде
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
