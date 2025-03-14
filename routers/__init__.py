4bd5876699fc162ff778606823bd626aa2043ba0
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-03-15 00:35:18 +1000

    Эндпоинты аутентификации

--- routers/__init__.py
+++ routers/__init__.py
@@ -1,6 +1,4 @@
 from .auth import router as auth_router
 from .health import router as health_router
-from .texts import router as text_router
-from .audio import router as audio_router
 
-__all__ = ("health_router", "auth_router", "text_router", "audio_router")
+__all__ = ("health_router", "auth_router")
--- routers/auth.py
+++ routers/auth.py
@@ -1,13 +1,50 @@
-from fastapi import APIRouter, Body
+import httpx
+from fastapi import APIRouter, Body, HTTPException
 
-from schemas.auth import AuthRequest, AuthResponse
+from configs import configs
+from schemas.auth import (
+    AuthenticateUserRequest,
+    AuthenticateUserResponse,
+    RegisterUserRequest,
+    RegisterUserResponse,
+)
 
 router = APIRouter(prefix="/auth")
 
 
-@router.post("", summary="Authenticate User", tags=["Auth"])
-def athenticate_user(user_data: AuthRequest = Body()) -> AuthResponse:
-    """Производит аутентификацию пользователя по его имени и паролю.
-    Возвращает пару refresh и acess токенов.
-    """
-    pass
+@router.post("/login", summary="Аутентификация пользователя", tags=["Auth"])
+async def athenticate_user(user_data: AuthenticateUserRequest) -> AuthenticateUserResponse:
+    """Аутентифицирует пользователя в системе ILPS. Возвращает JWT токен доступа в случае успеха."""
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.post(
+                f"{configs.services.auth.URL}/login",
+                content=user_data.model_dump_json(),
+            )
+            response.raise_for_status()
+        except httpx.HTTPStatusError as e:
+            raise HTTPException(
+                status_code=e.response.status_code,
+                detail=e.response.json().get("detail", "Unknown error"),
+            )
+
+        return AuthenticateUserResponse(**response.json())
+
+
+@router.post("/register", summary="Регистрация пользователя", tags=["Auth"])
+async def register_user(user_data: RegisterUserRequest = Body()) -> RegisterUserResponse:
+    """Регистрирует нового пользователя в системе ILPS."""
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.post(
+                f"{configs.services.auth.URL}/register",
+                content=user_data.model_dump_json(),
+            )
+            response.raise_for_status()
+        except httpx.HTTPStatusError as e:
+            raise HTTPException(
+                status_code=e.response.status_code,
+                detail=e.response.json().get("detail", "Unknown error"),
+            )
+
+        return RegisterUserResponse(**response.json())
