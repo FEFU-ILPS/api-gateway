c76acc6395417a6712a3eeab6503fd904d12a648
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-04-06 19:39:40 +1000

    feature: Подключение роутера текстов

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from routers import auth_router, health_router
+from routers import auth_router, health_router, texts_router
 
 
 @asynccontextmanager
@@ -16,8 +16,10 @@ async def lifespan(_: FastAPI):
 
 
 gateway = FastAPI(lifespan=lifespan)
+
 gateway.include_router(health_router)
 gateway.include_router(auth_router)
+gateway.include_router(texts_router)
 
 gateway.add_middleware(
     CORSMiddleware,
