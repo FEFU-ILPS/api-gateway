04764a997ab3592ea806594e2f5527c4d7732c49
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-04-10 21:36:28 +1000

    feature: Роуты CRUD для текстов

--- routers/texts.py
+++ routers/texts.py
@@ -2,10 +2,18 @@ from typing import Annotated, List
 from uuid import UUID
 
 import httpx
-from fastapi import APIRouter, Path, HTTPException
+from fastapi import APIRouter, Path, HTTPException, Body
 
 from configs import configs
-from schemas.texts import DetailLearningTextResponse, LearningTextResponse
+from schemas.texts import (
+    DetailLearningTextResponse,
+    LearningTextResponse,
+    DeleteLearningTextResponse,
+    CreateLearningTextRequest,
+    CreateLearningTextResponse,
+    UpdateLearningTextRequest,
+    UpdateLearningTextResponse,
+)
 
 router = APIRouter(prefix="/texts")
 
@@ -42,3 +50,67 @@ async def get_text(uuid: Annotated[UUID, Path(...)]) -> DetailLearningTextRespon
             )
 
     return DetailLearningTextResponse(**response.json())
+
+
+@router.post("/", summary="Добавить текст в систему")
+async def create_text(
+    data: Annotated[CreateLearningTextRequest, Body(...)],
+) -> CreateLearningTextResponse:
+    """Добавляет новый текст в систему."""
+
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.post(
+                f"{configs.services.texts.URL}/",
+                content=data.model_dump(exclude_none=True),
+            )
+            response.raise_for_status()
+
+        except httpx.HTTPStatusError as e:
+            raise HTTPException(
+                status_code=e.response.status_code,
+                detail=e.response.json().get("detail", "Unknown error"),
+            )
+
+    return CreateLearningTextResponse(**response.json())
+
+
+@router.delete("/{uuid}", summary="Удалить текст из системы")
+async def delete_text(uuid: Annotated[UUID, Path(...)]) -> DeleteLearningTextResponse:
+    """Удаляет текст из системы по его UUID."""
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.delete(f"{configs.services.texts.URL}/{uuid}")
+            response.raise_for_status()
+
+        except httpx.HTTPStatusError as e:
+            raise HTTPException(
+                status_code=e.response.status_code,
+                detail=e.response.json().get("detail", "Unknown error"),
+            )
+
+    return DetailLearningTextResponse(**response.json())
+
+
+@router.patch("/{uuid}", summary="Обновить данные о тексте")
+async def update_text(
+    uuid: Annotated[UUID, Path(...)],
+    data: Annotated[UpdateLearningTextRequest, Body(...)],
+) -> UpdateLearningTextResponse:
+    """Обновляет данные текста по его UUID."""
+
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.patch(
+                f"{configs.services.texts.URL}/{uuid}",
+                content=data.model_dump(exclude_none=True),
+            )
+            response.raise_for_status()
+
+        except httpx.HTTPStatusError as e:
+            raise HTTPException(
+                status_code=e.response.status_code,
+                detail=e.response.json().get("detail", "Unknown error"),
+            )
+
+    return UpdateLearningTextResponse(**response.json())
