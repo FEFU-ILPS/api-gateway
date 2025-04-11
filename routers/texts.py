087bbd3cdc76cb5d2c250ca188cc3b55957e314a
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-04-11 20:58:28 +1000

    feature: Улучшение листинга текстов (closes #4)

--- routers/texts.py
+++ routers/texts.py
@@ -1,29 +1,56 @@
+from enum import Enum
 from typing import Annotated, List
 from uuid import UUID
 
 import httpx
-from fastapi import APIRouter, Path, HTTPException, Body
+from fastapi import APIRouter, Body, Depends, HTTPException, Path
 
 from configs import configs
 from schemas.texts import (
-    DetailLearningTextResponse,
-    LearningTextResponse,
-    DeleteLearningTextResponse,
     CreateLearningTextRequest,
     CreateLearningTextResponse,
+    DeleteLearningTextResponse,
+    DetailLearningTextResponse,
+    LearningTextResponse,
     UpdateLearningTextRequest,
     UpdateLearningTextResponse,
 )
 
+from .utils.query_params import ListingPagination, ListingSearch, ListingSort
+
 router = APIRouter(prefix="/texts")
 
 
+class SortFields(str, Enum):
+    TITLE = "title"
+    DIFFICULTY = "difficulty"
+
+
+class SearchFields(str, Enum):
+    TITLE = "title"
+    DIFFICULTY = "difficulty"
+    PREVIEW = "preview"
+
+
 @router.get("/", summary="Получить список всех текстов")
-async def get_texts() -> List[LearningTextResponse]:
+async def get_texts(
+    search: ListingSearch[SearchFields] = Depends(),
+    sort: ListingSort[SortFields] = Depends(),
+    pagination: ListingPagination = Depends(),
+) -> List[LearningTextResponse]:
     """Возвращает полный список всех обучающих текстов с краткой информацией."""
     async with httpx.AsyncClient() as client:
         try:
-            response = await client.get(f"{configs.services.texts.URL}/")
+            response_params = {}
+            request_params = (search, sort, pagination)
+
+            for group in request_params:
+                response_params.update(group.model_dump(exclude_none=True))
+
+            response = await client.get(
+                f"{configs.services.texts.URL}/",
+                params=response_params,
+            )
             response.raise_for_status()
 
         except httpx.HTTPStatusError as e:
