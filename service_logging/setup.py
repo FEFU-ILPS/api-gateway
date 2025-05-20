8df2e0c19e54daaa5cae95f14e73de5590b8818a
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-05-20 19:12:08 +1000

    fix: Корректная настройка отправки в Graylog

--- service_logging/setup.py
+++ service_logging/setup.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import json
 import sys
 
 import graypy
@@ -8,31 +7,6 @@ import loguru
 from loguru import logger
 
 from configs import configs
-
-
-def serialize_record(message: loguru.Message) -> str:
-    """Функция-сериализатор для логов loguru в GELF формат.
-
-    Args:
-        message (loguru.Message): Объект сообщения лога loguru.
-
-    Returns:
-        str: JSON строка формата лога.
-    """
-    record = message.record
-    return json.dumps(
-        {
-            "timestamp": record["time"].timestamp(),
-            "level": record["level"].name,
-            "module": record["module"],
-            "file": record["file"].name,
-            "line": record["line"],
-            "message": record["message"],
-            "service": record["extra"].get("service", "unknown"),
-            "request_hash": record["extra"].get("request_hash", None),
-            "exception": record["exception"],
-        }
-    )
 
 
 def loguru_formatter(record: loguru.Record) -> str:
@@ -53,7 +27,7 @@ def loguru_formatter(record: loguru.Record) -> str:
     # Формат: дата | время | сервис | уровень | файл:строка - сообщение (reques_id)
     return (
         "<green>{time:DD-MM-YYYY}</green> | "
-        "<green>{time:ss:mm:HH}</green> | "
+        "<green>{time:HH:mm:ss}</green> | "
         "<cyan>{extra[service]}</cyan> | "
         "<level>{level: <8}</level> | "
         f"{hash_id}"
@@ -88,6 +62,12 @@ def setup_logger() -> loguru.Logger:
             configs.graylog.HOST,
             configs.graylog.PORT,
         )
-        logger.add(gelf_handler, format=serialize_record)
+        logger.add(
+            sink=gelf_handler,
+            format=loguru_formatter,
+            level="DEBUG",
+            backtrace=True,
+            diagnose=True,
+        )
 
     return logger.bind(service=configs.SERVICE_NAME)
