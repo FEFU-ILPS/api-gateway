0d04489de1d584beb1ea0ac6c7da44a297055986
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-05-20 12:48:53 +1000

    feature: Конфигурация Graylog для сервиса

--- configs/__init__.py
+++ configs/__init__.py
@@ -1,6 +1,7 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from .services import ServicesConfiguration
+from .graylog import GraylogConfiguration
 
 
 class ProjectConfiguration(BaseSettings):
@@ -8,6 +9,7 @@ class ProjectConfiguration(BaseSettings):
 
     # * Вложенные группы настроек
     services: ServicesConfiguration = ServicesConfiguration()
+    graylog: GraylogConfiguration = GraylogConfiguration()
 
     # * Опциональные переменные
     DEBUG_MODE: bool = False
---
+++ configs/graylog.py
@@ -0,0 +1,12 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class GraylogConfiguration(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="GATEWAY_GRAYLOG_")
+
+    # ! Обязательные переменные
+    HOST: str
+    PORT: int
+
+    # * Опциональные переменные
+    ENABLE: bool = True
